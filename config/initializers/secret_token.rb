19846ea25e2f236c223843c1b96f19112650a65d
John Mannix <mannixjp@muohio.edu>
2012-04-04 14:28:21 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clixdb::Application.config.secret_token = '3bcd68ca033fea30f6d72d7e940b64845451d857887c9ce1acf9dafdf0a050cc2cc7a957331118147caee5c64f23617c822cfbb5e60a5dbf0d3d646883e09503'
